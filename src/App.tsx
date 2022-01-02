e08a2a4a463620eac548fba28982f038a1a4643c
itsidorkin28 <itsidorkin28@gmail.com>
2022-01-02 20:57:41 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import { v1 } from 'uuid';
 import './App.css';
 
+type MessagesType = Array<{
+    userId: number
+    photo: string
+    message: string
+    userName: string
+}>
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState<string>('')
+    const [messages, setMessages] = useState<MessagesType>([])
+    const [socket, setSocket] = useState<WebSocket | null>(null)
+    useEffect(() => {
+        let socket = new WebSocket('wss://social-network.samuraijs.com/handlers/chatHandler.ashx')
+        setSocket(socket)
+        socket.onmessage = function (e: MessageEvent) {
+            setMessages(state => [...state, ...JSON.parse(e.data)])
+        }
+    }, [])
+
+
+    const send = () => {
+        socket?.send(value)
+    }
+    return (
+        <div className="App">
+            <div>
+                {messages.map(m => <div key={v1()}>
+                    <img src={m.photo}/>
+                    <strong>{m.userName}</strong>
+                    {m.message}
+                </div>)}
+            </div>
+            <div>
+                <textarea value={value} onChange={(e) => setValue(e.currentTarget.value)}/>
+            </div>
+            <div>
+                <button onClick={send}>Send</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
+
+
